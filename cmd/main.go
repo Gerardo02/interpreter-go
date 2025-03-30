55ca713c08aea4e6ea43a7494b86aa6ab3b77295
Gerardo02 <gerardo.jerez@hotmail.com>
2025-03-30 01:45:16 -0700

    lexer done (no strings yet)

--- cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Print("no lo haga compa")
-}
--- lexer/lexer.go
+++ lexer/lexer.go
@@ -23,7 +23,30 @@ func (l *Lexer) NextToken() token.Token {
 	l.skipWhitespace()
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+
+	case '<':
+		tok = newToken(token.LT, l.ch)
+
+	case '>':
+		tok = newToken(token.GT, l.ch)
 
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
@@ -46,6 +69,15 @@ func (l *Lexer) NextToken() token.Token {
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
 
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -53,7 +85,7 @@ func (l *Lexer) NextToken() token.Token {
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
-			tok.Type = token.LookUpIdent(tok.Literal)
+			tok.Type = token.LookUpIdent(tok.Literal) // look if is a keyword
 			return tok
 		} else if isDigit(l.ch) {
 			tok.Type = token.INT
@@ -109,5 +141,13 @@ func (l *Lexer) readChar() {
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
+	}
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
 	}
 }
