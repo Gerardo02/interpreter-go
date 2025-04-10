bc2086eb6cf54e553ee1bd9a0d09f839f872c574
Gerardo02 <gerardo.jerez@hotmail.com>
2025-04-09 17:46:47 -0700

    parsing let statements (no expressions)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,18 +9,16 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-		let x = 5;
+		let x 5;
+		let foobar 838383;
 		let y = 10;
-		let foobar = 838383;
 	`
 
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
-	if program == nil {
-		t.Fatalf("ParseProgram() returned nil")
-	}
+	checkParseErrors(t, p)
 
 	if len(program.Statements) != 3 {
 		t.Fatalf(
@@ -67,4 +65,17 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParseErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
